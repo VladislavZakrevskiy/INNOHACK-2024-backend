1492085991d60cb7c09f6c8545c9ffa71f102adc
Timur <110349339+T1murCoder@users.noreply.github.com>
2024-09-28 12:32:59 +0300

    added getUserTasks method

--- ProjectService/src/main/java/origin/repository/TaskRepository.java
+++ ProjectService/src/main/java/origin/repository/TaskRepository.java
@@ -1,6 +1,7 @@
 package origin.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import origin.model.task.Task;
 
@@ -8,4 +9,5 @@ import java.util.List;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByExecutorId(long id);
 }
